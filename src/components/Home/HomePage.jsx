e82d0bc817a67e4077acd729c2d42a5e68571120
Sanjay Gupta <sanjayg.8237@gmail.com>
2022-09-29 00:08:22 +0530

    all routes updated

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,52 +1,80 @@
-import { Box, Flex, Grid, GridItem, Image, Input, InputGroup, InputLeftAddon, Stack } from '@chakra-ui/react'
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Navbar from './Navbar'
+import {
+  Box,
+  Flex,
+  Grid,
+  GridItem,
+  Image,
+  Input,
+  InputGroup,
+  InputLeftAddon,
+  Stack,
+} from "@chakra-ui/react";
+import React from "react";
+import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
 
 function HomePage() {
   return (
-    <div><Link to="/homepage"></Link>
-        <Navbar />
-        
-            <Box w='100%' bg='#fff0b8' height={"500px"} >
+    <div>
+      <Link to="/homepage"></Link>
+      <Navbar />
 
-                <Flex alignItems={"center"} gap={100} p={30}>
-                    <Box w="40%" height={"400px"} >
-                        <h1>Make the best use of your time</h1>
-                        <p>TMetric is a smart choice for time tracking to see work in progress and keep team, cost and quality under control</p>
-                        
+      <Box w="100%" bg="#fff0b8" height={"500px"}>
+        <Flex alignItems={"center"} gap={100} p={30}>
+          <Box w="40%" height={"400px"}>
+            <h1>Make the best use of your time</h1>
+            <p>
+              TMetric is a smart choice for time tracking to see work in progress and keep team,
+              cost and quality under control
+            </p>
+          </Box>
+          <Box w="45%" height={"400px"}>
+            <img src="https://tmetric.com/media/w53hmkzd/img-video-cover.png" alt="" />
+          </Box>
+        </Flex>
+      </Box>
 
-                    </Box>
-                    <Box w="45%" height={"400px"} >
-                        <img src="https://tmetric.com/media/w53hmkzd/img-video-cover.png" alt="" />
-                    </Box>
-                </Flex>
-            </Box>
-
-            
-            <Stack direction='row' justifyContent={"center"} gap={40}>
-                <Image
-                    boxSize='150px'
-                    objectFit='cover'
-                    src='https://tmetric.com/media/zlalggkp/saasworthy_2021_main_page.png'
-                    alt='Dan Abramov'
-                />
-                <Image
-                    boxSize='150px'
-                    objectFit='cover'
-                    src='https://tmetric.com/media/w4mhgaul/time-tracking-software.png'
-                    alt='Dan Abramov'
-                />
-                <Image boxSize='150px' src='https://tmetric.com/media/mg3fi1e4/crozdesk_2022_main_page.png' alt='Dan Abramov' />
-                <Image boxSize='150px' src='https://tmetric.com/media/c2eewysw/g2_2022_main_page.png' alt='Dan Abramov' />
-                <Image boxSize='150px' src='https://tmetric.com/media/a00doqsb/icon-customer-rating.png' alt='Dan Abramov' />
-                <Image boxSize='150px' src='https://tmetric.com/media/0mxfl3xw/icon-customer-choice.png' alt='Dan Abramov' />
-                <Image boxSize='150px' src='https://tmetric.com/media/jfxavlps/getapp-logotype.svg' alt='Dan Abramov' />
-            </Stack>
-            
-        
+      <Stack direction="row" justifyContent={"center"} gap={40}>
+        <Image
+          boxSize="150px"
+          objectFit="cover"
+          src="https://tmetric.com/media/zlalggkp/saasworthy_2021_main_page.png"
+          alt="Dan Abramov"
+        />
+        <Image
+          boxSize="150px"
+          objectFit="cover"
+          src="https://tmetric.com/media/w4mhgaul/time-tracking-software.png"
+          alt="Dan Abramov"
+        />
+        <Image
+          boxSize="150px"
+          src="https://tmetric.com/media/mg3fi1e4/crozdesk_2022_main_page.png"
+          alt="Dan Abramov"
+        />
+        <Image
+          boxSize="150px"
+          src="https://tmetric.com/media/c2eewysw/g2_2022_main_page.png"
+          alt="Dan Abramov"
+        />
+        <Image
+          boxSize="150px"
+          src="https://tmetric.com/media/a00doqsb/icon-customer-rating.png"
+          alt="Dan Abramov"
+        />
+        <Image
+          boxSize="150px"
+          src="https://tmetric.com/media/0mxfl3xw/icon-customer-choice.png"
+          alt="Dan Abramov"
+        />
+        <Image
+          boxSize="150px"
+          src="https://tmetric.com/media/jfxavlps/getapp-logotype.svg"
+          alt="Dan Abramov"
+        />
+      </Stack>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
--- src/route/AllRoutes.jsx
+++ src/route/AllRoutes.jsx
@@ -2,23 +2,25 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import About from "../components/Blogs/BlogHome/About";
 import GuestPost from "../components/Blogs/BlogHome/GuestPost";
-import Home from "../components/Blogs/BlogHome/Home";
+// import Home from "../components/Blogs/BlogHome/Home";
 import News from "../components/Blogs/BlogHome/News";
 import Productivity from "../components/Blogs/BlogHome/Productivity";
 import Search from "../components/Blogs/BlogHome/Search";
 import UseCases from "../components/Blogs/BlogHome/UseCases";
 import Navbar from "../components/Blogs/BlogNavbar/Navbar";
+import HomePage from "../components/Home/HomePage";
 
 function AllRoutes() {
   return (
     <div>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/price" element={<h1>Price</h1>} />
         <Route path="/support" element={<h1>Support</h1>} />
         <Route path="/blog" element={<h1>Blog</h1>} />
         <Route path="/login" element={<h1>Login</h1>} />
         <Route path="/signup" element={<h1>Signup</h1>} />
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
         <Route path="/news" element={<News />} />
         <Route path="/productivity" element={<Productivity />} />
         <Route path="/usecases" element={<UseCases />} />
