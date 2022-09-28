56f967f6e550bdfe92273fbe2bb2bbde35b16b94
vkaware <vaibhavkaware2@gmail.com>
2022-09-28 21:55:41 +0530

    day2

---
+++ src/route/AllRoutes.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Route, Routes } from "react-router-dom";
+import HomePage from '../components/HomePage';
+
+function AllRoutes() {
+  return (
+    <div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/price" element={<h1>Price</h1>} />
+      <Route path="/support" element={<h1>Support</h1>} />
+      <Route path="/blog" element={<h1>Blog</h1>} />
+      <Route path="/login" element={<h1>Login</h1>} />
+      <Route path="/signup" element={<h1>Signup</h1>} />
+      
+    </Routes>
+    </div>
+  )
+}
+
+export default AllRoutes
