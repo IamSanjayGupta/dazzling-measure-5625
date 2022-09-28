d602c28dffb187a27930ff2c197d0b6eea9cd6e4
Sanjay Gupta <sanjayg.8237@gmail.com>
2022-09-28 23:42:12 +0530

    resoving merge issue

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-
 import "./App.css";
 import Task from "./components/Task/Task";
-import Navbar from './components/Blogs/BlogNavbar/Navbar';
+import Navbar from "./components/Blogs/BlogNavbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Task />
-    <Navbar/>
     </div>
   );
 }
