3b8101388646d0f658ddf2bff7e7b423583952ef
Sanjay Gupta <sanjayg.8237@gmail.com>
2022-09-29 20:23:25 +0530

    task list code moved to task component

--- src/components/Task/TaskContainer.jsx
+++ src/components/Task/TaskContainer.jsx
@@ -1,32 +1,20 @@
 import {
-  Accordion,
-  AccordionButton,
-  AccordionIcon,
-  AccordionItem,
-  AccordionPanel,
-  Avatar,
+  HStack,
   Box,
   Button,
   Divider,
-  HStack,
-  IconButton,
   Input,
   InputGroup,
   InputLeftElement,
-  Tooltip,
   useDisclosure,
-  VStack,
 } from "@chakra-ui/react";
-import { AddIcon, CheckIcon, SearchIcon } from "@chakra-ui/icons";
-import { FiPlay } from "react-icons/fi";
-import { FaPlay } from "react-icons/fa";
-import { RiDeleteBin6Line } from "react-icons/ri";
-import { TbArrowBarToRight } from "react-icons/tb";
-import { BsCheckCircle, BsShare } from "react-icons/bs";
+import { AddIcon, SearchIcon } from "@chakra-ui/icons";
+
 import React from "react";
 import SearchFilterButton from "./SearchFilterButton";
 import { filterBtn } from "../../utils/filterBtn";
 import NewTask from "./NewTask";
+import TaskList from "./TaskList";
 
 const TaskContainer = () => {
   const { isOpen, onOpen, onClose } = useDisclosure(true);
@@ -46,51 +34,7 @@ const TaskContainer = () => {
       </HStack>
       <Divider my="4" />
       <HStack alignItems="flex-start">
-        <Accordion allowToggle w="100%">
-          <AccordionItem>
-            <AccordionButton>
-              <AccordionIcon />
-              <Box ml="2">Section 1</Box>
-            </AccordionButton>
-            <AccordionPanel p="0">
-              <HStack
-                size="md"
-                gap="2"
-                px="3"
-                _hover={{ bg: "gray.100" }}
-                _active={{ bg: "gray.100" }}
-                onClick={onOpen}
-              >
-                <IconButton
-                  colorScheme="gray"
-                  variant="outline"
-                  rounded="xl"
-                  border="none"
-                  icon={<BsCheckCircle />}
-                />
-                <Input
-                  type="text"
-                  defaultValue="Learn React"
-                  border="none"
-                  size="sm"
-                  _focus={{ bgColor: "white" }}
-                />
-                <HStack>
-                  <Tooltip label="Sanjay" aria-label="A tooltip">
-                    <Avatar size="xs" name="Sanjay" src="https://bit.ly/dan-abramov" />
-                  </Tooltip>
-                  <IconButton
-                    colorScheme="gray"
-                    variant="outline"
-                    rounded="xl"
-                    border="none"
-                    icon={<FiPlay />}
-                  />
-                </HStack>
-              </HStack>
-            </AccordionPanel>
-          </AccordionItem>
-        </Accordion>
+        <TaskList onOpen={onOpen} />
         {isOpen ? <NewTask onClose={onClose} /> : ""}
       </HStack>
     </Box>
