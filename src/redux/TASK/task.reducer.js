a728d483eb8a777f457b0ed89722c6991d554eca
Sanjay Gupta <sanjayg.8237@gmail.com>
2022-09-30 20:32:58 +0530

    Task reducer created

---
+++ src/redux/TASK/task.reducer.js
@@ -0,0 +1,22 @@
+import * as action from "./task.types";
+const initState = {
+  loading: false,
+  error: false,
+  tasks: [],
+};
+
+export const task = (state = initState, { type, payload }) => {
+  switch (type) {
+    case action.TASK_LOADING:
+      return { loading: true, error: false };
+    case action.TASK_ERROR:
+      return { loading: false, error: true };
+    case action.TASK_ADD:
+      return { loading: false, error: false, tasks: [...state.tasks, payload] };
+    case action.TASK_UPDATE:
+      return { loading: false, error: false };
+
+    default:
+      return state;
+  }
+};
---
+++ src/redux/TASK/task.types.js
@@ -0,0 +1,4 @@
+export const TASK_LOADING = "TASK_LOADING";
+export const TASK_ERROR = "TASK_ERROR";
+export const TASK_ADD = "TASK_ADD";
+export const TASK_UPDATE = "TASK_UPDATE";
