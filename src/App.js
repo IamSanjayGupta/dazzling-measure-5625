bdc48e6f1e6d46d99730f4823f15808bd86fbc6d
Sanjay Gupta <sanjayg.8237@gmail.com>
2022-10-01 19:54:33 +0530

    removed unwanted code from app js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import AllRoutes from "./route/AllRoutes";
-import MainApp from "./components/MainApp";
 
 function App() {
   return (
     <div className="App">
-      {/* <AllRoutes /> */}
-      <MainApp />
+      <AllRoutes />
     </div>
   );
 }
